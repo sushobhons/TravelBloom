7fe9fa910f4db5af16fc92c4a6c5184134e1366c
Sushobhon Sahoo <sushobhons@gmail.com>
2024-12-04 09:34:58 -0500

    add search functionality

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,18 +1,70 @@
 const btnSearch = document.getElementById("btnSearch");
 const btnResetSearch = document.getElementById("btnResetSearch");
 
-function searchDestination(){
+function searchDestination() {
     const input = document.getElementById("destinationInput").value.toLowerCase();
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = '';
 
     fetch('travel_recommendation_api.json')
-    .then( response => response.json())
-    .then( data =>{
+        .then(response => response.json())
+        .then(data => {
 
-    })
-    .catch(error =>{
-        console.log('Error:', error);
-        resultDiv.innerHTML = 'An error occurred while fetching data.';
-    });
+            if (input.includes('beach')) {
+                const beaches = data.beaches;
+                beaches.forEach(beach => {
+                    resultDiv.innerHTML += `
+                    <div class="result-item">
+                    <div class="image-section"> 
+                    <img src="${beach.imageUrl}" alt="${beach.name}">
+                    </div><div class="text-section">
+                    <h2>${beach.name}</h2>
+                    <p>${beach.description}</p>
+                    </div>
+                    </div>`;
+                });
+            }
+
+            if (input.includes('temple')) {
+                const temples = data.temples;
+                temples.forEach(temple => {
+                    resultDiv.innerHTML += `
+                    <div class="result-item">
+                    <div class="image-section"> 
+                    <img src="${temple.imageUrl}" alt="${temple.name}">
+                    </div><div class="text-section">
+                    <h2>${temple.name}</h2>
+                    <p>${temple.description}</p>
+                    </div>
+                    </div>`;
+                });
+            }
+
+            const countries = data.countries;
+
+            countries.forEach(country => {
+                if (country.name.toLowerCase().includes(input)) {
+                    country.cities.forEach(city => {
+                        resultDiv.innerHTML += `
+                    <div class="result-item">
+                    <div class="image-section"> 
+                    <img src="${city.imageUrl}" alt="${city.name}">
+                    </div><div class="text-section">
+                    <h2>${city.name}</h2>
+                    <p>${city.description}</p>
+                    </div>
+                    </div>`;
+                    });
+                }
+
+            });
+
+
+        })
+        .catch(error => {
+            console.log('Error:', error);
+            resultDiv.innerHTML = 'An error occurred while fetching data.';
+        });
 }
+
+btnSearch.addEventListener('click', searchDestination);
