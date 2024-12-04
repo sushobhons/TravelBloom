37fa8f8725dfa6e903821e5319a42f7fc5359c3c
Sushobhon Sahoo <sushobhons@gmail.com>
2024-12-04 14:24:08 -0500

    Add style

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -10,55 +10,56 @@ function searchDestination() {
         .then(response => response.json())
         .then(data => {
 
-            if (input.includes('beach')) {
-                const beaches = data.beaches;
-                beaches.forEach(beach => {
-                    resultDiv.innerHTML += `
-                    <div class="result-item">
-                    <div class="image-section"> 
-                    <img src="${beach.imageUrl}" alt="${beach.name}">
-                    </div><div class="text-section">
-                    <h2>${beach.name}</h2>
-                    <p>${beach.description}</p>
-                    </div>
-                    </div>`;
-                });
-            }
-
-            if (input.includes('temple')) {
-                const temples = data.temples;
-                temples.forEach(temple => {
-                    resultDiv.innerHTML += `
-                    <div class="result-item">
-                    <div class="image-section"> 
-                    <img src="${temple.imageUrl}" alt="${temple.name}">
-                    </div><div class="text-section">
-                    <h2>${temple.name}</h2>
-                    <p>${temple.description}</p>
-                    </div>
-                    </div>`;
-                });
-            }
-
-            const countries = data.countries;
-
-            countries.forEach(country => {
-                if (country.name.toLowerCase().includes(input)) {
-                    country.cities.forEach(city => {
+            if (input !== '') {
+                if (input.includes('beach')) {
+                    const beaches = data.beaches;
+                    beaches.forEach(beach => {
                         resultDiv.innerHTML += `
-                    <div class="result-item">
-                    <div class="image-section"> 
-                    <img src="${city.imageUrl}" alt="${city.name}">
-                    </div><div class="text-section">
-                    <h2>${city.name}</h2>
-                    <p>${city.description}</p>
-                    </div>
-                    </div>`;
+                        <div class="result-item">
+                        <div class="image-section"> 
+                        <img src="${beach.imageUrl}" alt="${beach.name}">
+                        </div><div class="text-section">
+                        <h2>${beach.name}</h2>
+                        <p>${beach.description}</p>
+                        </div>
+                        </div>`;
                     });
                 }
 
-            });
+                if (input.includes('temple')) {
+                    const temples = data.temples;
+                    temples.forEach(temple => {
+                        resultDiv.innerHTML += `
+                        <div class="result-item">
+                        <div class="image-section"> 
+                        <img src="${temple.imageUrl}" alt="${temple.name}">
+                        </div><div class="text-section">
+                        <h2>${temple.name}</h2>
+                        <p>${temple.description}</p>
+                        </div>
+                        </div>`;
+                    });
+                }
 
+                const countries = data.countries;
+
+                countries.forEach(country => {
+                    if (country.name.toLowerCase().includes(input)) {
+                        country.cities.forEach(city => {
+                            resultDiv.innerHTML += `
+                        <div class="result-item">
+                        <div class="image-section"> 
+                        <img src="${city.imageUrl}" alt="${city.name}">
+                        </div><div class="text-section">
+                        <h2>${city.name}</h2>
+                        <p>${city.description}</p>
+                        </div>
+                        </div>`;
+                        });
+                    }
+
+                });
+            }
 
         })
         .catch(error => {
@@ -68,3 +69,10 @@ function searchDestination() {
 }
 
 btnSearch.addEventListener('click', searchDestination);
+
+function resetSearch() {
+    document.getElementById("destinationInput").value = "";
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+}
+btnResetSearch.addEventListener('click', resetSearch);
